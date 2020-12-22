0280366253f830399f69fb90e3fe6eaba6960f1e
Kuei <ragnakuei@livemail.tw>
2020-12-22 10:42:09 +0800

    Add project files.

---
+++ EfCoreMigrationInHostedService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EfCoreMigrationInHostedService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
